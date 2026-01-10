46fff1a778c52b3304da62935447f1c5407011f5
TheQuietVoid <mauro_rossetti2@hotmail.com>
2026-01-10 17:24:07 +0100

    corretto load del form login

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-
-    window.addEventListener("load", (event) => {
-        $.get("static/html/login.html", function(data){
-            document.getElementById("main_content").innerHTML=data
+window.addEventListener("DOMContentLoaded", function(){
+    $.get("static/html/login.html", function(data){
+        document.getElementById("main_content").innerHTML=data
+        const btnLogin = document.getElementById("btnLogin")
+        btnLogin.addEventListener("click", function(){
+            alert("ciao")
         });
     });
+});
+$(document).ready(function() {
+    console.info("pagina principale")
 
 });
